4809a35b54bb072ecb9592d3481b3acab20242f0
Justin-mario <ppaddy766@gmail.com>
2025-02-26 08:53:34 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect
+
+app = Flask("__name__")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+@app.route("/about")
+def about_us():
+    return render_template("about_us.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
